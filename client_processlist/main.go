f1bc675ca1a21bc4f8e1fb132e7b1f1743fe3d8c
wqmysqldba@126.com <wqmysqldba@126.com>
2022-08-23 17:42:13 +0800

    完成证书加密功能

--- client_processlist/main.go
+++ client_processlist/main.go
@@ -2,18 +2,28 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"github.com/songzhibin97/gkit/tools/pretty"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 	"time"
 	"workgrpc/pb"
 )
-func NewShowProcessListTask()*pb.ShowProcesslistRequest{
+
+func NewShowProcessListTask() *pb.ShowProcesslistRequest {
 	return &pb.ShowProcesslistRequest{
 		ShowMsg: "",
 	}
 }
-func main()  {
-	conn, err := grpc.Dial("127.0.0.1:3000", grpc.WithInsecure())
+
+func main() {
+	//creditsServeKey := "/Users/anderalex/go/src/workgrpc/certify/server.key"
+	creditsServePem := "/Users/anderalex/go/src/workgrpc/certify/server.crt"
+	commandName := "example.server.com"
+	creds, _ := credentials.NewClientTLSFromFile(creditsServePem, commandName)
+
+	conn, err := grpc.Dial("127.0.0.1:3000", grpc.WithTransportCredentials(creds))
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -21,6 +31,16 @@ func main()  {
 	ctx, cancle := context.WithTimeout(context.Background(), time.Second)
 	defer cancle()
 
-	res,err:=client.NewShowProcesslist(ctx,NewShowProcessListTask())
-	fmt.Println(res.ProcessListInfo)
+	res, err := client.NewShowProcesslist(ctx, NewShowProcessListTask())
+	if err != nil {
+		fmt.Println(err)
+	}
+	if res != nil {
+		strbyte, e := json.Marshal(res)
+		if e != nil {
+			fmt.Println(e)
+		}
+		// strbyte 是 []byte 类型，可以直接通过接口 func()gin.H{} 返回给前端json数组
+		fmt.Printf("%s\n", pretty.Pretty(strbyte))
+	}
 }
--- server/main.go
+++ server/main.go
@@ -3,21 +3,30 @@ package main
 import (
 	"fmt"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+
 	"log"
 	"net"
 	"workgrpc/pb"
 )
-
 func main() {
+
+	creditsServeKey := "/Users/anderalex/go/src/workgrpc/certify/server.key"
+	creditsServeCrt := "/Users/anderalex/go/src/workgrpc/certify/server.crt"
+	//commandName := "GrpcMySQLServer"
+
+	creds, _ := credentials.NewServerTLSFromFile(creditsServeCrt, creditsServeKey)
+	s := grpc.NewServer(grpc.Creds(creds))
 	listen, err := net.Listen("tcp", ":3000")
 	if err != nil {
 		log.Panic("xxxxxx")
 	}
-	s := grpc.NewServer()
-	/* 备份恢复服务 */
+
+	/* 备份服务 */
 	pb.RegisterMySQLBackupServiceServer(s, &BackUpServer{})
 	/* Show Processlist 服务 */
 	pb.RegisterMySQLShowProcessListServiceServer(s, &ShowProcessListServer{})
+	/* 关键参数服务 */
 
 	// TODO 心跳表服务 读取到集群TOPO表中到mysql信息，然后记录心跳表 参考pt-heartbeat ，获取当前节点mysql的ip port，查询instance表中，该实例的角色信息，根据角色信息做读写心跳检测。 不支持单机多实例类型
 	if err := s.Serve(listen); err != nil {
